95ac76695672db4dd15ab39155336df7bc9ae50c
ckck912ck-lang <ckck912ck@gmail.com>
2026-07-15 13:48:52 +0900

    Add comments retrieval and display functionality to Profile component

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,11 +1,35 @@
 import { signOut } from "firebase/auth";
-import { authService } from "../firebase";
-import { Typography, Button } from "@mui/material";
+import { authService, db } from "../firebase";
+import { Typography, Button, Divider, List } from "@mui/material";
 import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
+import { collection, query, where, getDocs, orderBy, onSnapshot } from "firebase/firestore";
+import Comment from "../components/Comment";
 
 function Profile() {
   const auth = authService;
+  const userId = auth.currentUser.uid;
+
   const navigate = useNavigate();
+  const [comments, setComments] = useState([]);
+
+  const getComments = async () => {
+    const q = query(
+      collection(db, "comments"),
+      where("uid", "==", userId),
+      orderBy("date", "desc"),
+    );
+
+    onSnapshot(q, querySnapshot => {
+      const commentsArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+      setComments(commentsArray);
+    });
+  };
+
+  useEffect(() => {
+    getComments();
+  }, []);
 
   const onLogout = () => {
     signOut(auth)
@@ -18,12 +42,21 @@ function Profile() {
       });
   };
 
+  console.log(comments);
+
   return (
     <>
       <h2>Profile</h2>
       <Button sx={{ mt: 2 }} type="button" variant="contained" onClick={onLogout}>
         로그아웃
       </Button>
+      <Divider sx={{ my: 3 }} />
+      <List sx={{ width: "100%" }}>
+        {/* comments 배열의 값을 ListItem으로 출력 */}
+        {comments.map(item => (
+          <Comment key={item.id} item={item} isShown={userId === item.uid} />
+        ))}
+      </List>
     </>
   );
 }
